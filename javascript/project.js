baf8e8eb09ba456b1db07210c8bf36d1e2a447a9
leightracey1 <ltracey203@gmail.com>
2018-10-30 21:50:12 -0400

    changes

--- javascript/project.js
+++ javascript/project.js
@@ -43,6 +43,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
 $("#clicker").on("click", function(){
     $("body").css({"overflow":"visible"})
+    $(".line").css({
+        'position': 'relative',
+        'top': '50%', 
+        'margin': '0 auto',  
+        'border-right': '1px solid rgba(255,255,255, 0.75)', 
+        'font-size': '40px',  
+        'white-space': 'nowrap', 
+        'overflow': 'hidden', 'transform': 'translateY(-50%)'
+    })
+    $(".anim-typewriter").css({
+        'animation': 'typewriter 4s steps(28) 1s 1 normal both, blinkTextCursor 500ms steps(28) infinite normal'
+    })
     console.log("Button Clicked")
 })
 
